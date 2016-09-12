a322a59882eb28aa5c37f62922d2bc9c4e75cc8d
nnthhg <nnthhg@gmail.com>
2016-09-12 22:57:32 +0700

    Update

--- ConnectedLayer/MyConnectionFactory/Program.cs
+++ ConnectedLayer/MyConnectionFactory/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace MyConnectionFactory
 {
+    // A list of possible providers.
+    enum DataProvider
+    { SqlServer, OleDb, Odbc, None }
+
     class Program
     {
+        
         static void Main(string[] args)
         {
+            WriteLine("**** Very Simple Connection Factory *****\n");
+            // Get a specific connection.
+            IDbConnection myConnection = GetConnection(DataProvider.SqlServer);
+            WriteLine($"Your connection is a {myConnection.GetType().Name}");
+            // Open, use and close connection...
+            ReadLine();
+        }
+        // This method returns a specific connection object
+        // based on the value of a DataProvider enum.
+        static IDbConnection GetConnection(DataProvider dataProvider)
+        {
+            IDbConnection connection = null;
+            switch (dataProvider)
+            {
+                case DataProvider.SqlServer:
+                    connection = new SqlConnection();
+                    break;
+                case DataProvider.OleDb:
+                    connection = new OleDbConnection();
+                    break;
+                case DataProvider.Odbc:
+                    connection = new OdbcConnection();
+                    break;
+            }
+            return connection;
         }
     }
 }
