833e6fb9907b5091cdd249334d3c1a784acb3a76
nnthhg <nnthhg@icl.com>
2016-10-15 16:17:44 +0700

    Update

---
+++ ConnectedLayer/ConnectionObjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConnectionObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void ShowConnectionStatus(SqlConnection connection)
+        {
+            WriteLine("Info about your connection");
+            WriteLine($"Database");
+        }
+    }
+}
