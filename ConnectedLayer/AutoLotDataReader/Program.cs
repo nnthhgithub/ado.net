fe0ef23f31003a89d07f97fbd20bd6085770502e
nnthhg <nnthhg@icl.com>
2016-10-15 12:29:49 +0700

    AutoLotDataReader

---
+++ ConnectedLayer/AutoLotDataReader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace AutoLotDataReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = @"Data Source=.\sqlexpress;Initial Catalog=AutoLot;Integrated Security=True";
+                connection.Open();
+
+                string sql = "Select * from Inventory";
+                SqlCommand command = new SqlCommand(sql,connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        WriteLine($"-> Make: {reader["Make"]}, PetName: {reader["PetName"]}, Color: { reader["Color"]}.");
+                    }
+                }
+            }
+            ReadLine();
+        }
+    }
+}
