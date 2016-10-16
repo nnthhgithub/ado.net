f2c387aa67b3cf5f9f3325660b8b63899403bb38
nnthhg <nnthhg@icl.com>
2016-10-16 16:13:28 +0700

    CommandObjects project

---
+++ ConnectedLayer/CommandObjects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
